b5f14f87272bdccae2734d1fc417aaf976234e5f
Ramón Carnero Rojo <ramonmcr@gmail.com>
2015-02-24 12:58:15 +0100

    Initial Commit

---
+++ CameraWithShare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CameraWithShare
+//
+//  Created by Ramón Carnero Rojo on 24/2/15.
+//  Copyright (c) 2015 Ramón Carnero Rojo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
